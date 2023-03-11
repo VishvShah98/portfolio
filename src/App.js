e1979514a1dc800a2a9b10877347fae7513c2c2f
Vishv Shah <vishvshahwork@gmail.com>
2023-03-10 20:33:11 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "./styles.css";
 import LockScreen from "./components/lockscreen/lockscreen";
-
+import Main from "./main";
 import Slide from "./components/lockscreen/slideToUnlock";
 import { Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Slide />} />
-      <Route path="/lockscreen" element={<LockScreen />} />
+      {/* <Route path="/" element={<Slide />} /> */}
+      <Route path="/" element={<Main />} />
+      {/* <Route path="/lockscreen" element={<LockScreen />} /> */}
     </Routes>
   );
 }
